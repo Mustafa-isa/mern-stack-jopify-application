b65911773dd35d5bfec3f1fe2517292d5e90dc4a
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-06-08 07:10:48 +0300

    create user  tabel

--- controllers/AuthControler.js
+++ controllers/AuthControler.js
@@ -1,10 +1,19 @@
-const Register =async(req,res) =>{
-  res.send("register")
-}
-const Login =async(req,res) =>{
-  res.send("Login")
-}
-const Update =async(req,res) =>{
-  res.send("Update")
-}
-module.exports ={Register ,Login ,Update}
+const user = require("../modals/User");
+
+const Register = async (req, res) => {
+  try {
+    const userCreated = await user.create(req.body);
+    res.status(2001).json({ userCreated });
+  } catch (err) {
+    res.status(500).json({
+      msg: "error happened when create new user"
+    });
+  }
+};
+const Login = async (req, res) => {
+  res.send("Login");
+};
+const Update = async (req, res) => {
+  res.send("Update");
+};
+module.exports = { Register, Login, Update };
---
+++ modals/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "is required"],
+    minlength: 2,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validation: {
+      vaildate: validator.isEmail,
+      msg: "please provide valide email"
+    }
+  },
+  password: {
+    type: String,
+    required: [true, "is required"],
+    minlength: 8,
+    maxlength: 50
+  },
+  lastName: {
+    type: String,
+    defalult: "last name",
+    minlength: 2,
+    maxlength: 50
+  },
+  location: {
+    type: String,
+    defalult: "my city",
+    minlength: 2,
+    maxlength: 100
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
